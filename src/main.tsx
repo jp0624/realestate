fc26c5f8ba630d27786b05077dd2f282c40dd2d5
John Parker <jp0624@gmail.com>
2023-06-12 22:41:31 -0400

    init firebase config

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
+import { FirebaseAppProvider } from "reactfire"
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyDUDte02AohNMt74sNpI_AoDODMzzz2fIw",
+	authDomain: "jp0624-realestate.firebaseapp.com",
+	databaseURL: "https://jp0624-realestate-default-rtdb.firebaseio.com",
+	projectId: "jp0624-realestate",
+	storageBucket: "jp0624-realestate.appspot.com",
+	messagingSenderId: "582862176535",
+	appId: "1:582862176535:web:045283555fd7fa16867752",
+}
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<FirebaseAppProvider firebaseConfig={firebaseConfig}>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</FirebaseAppProvider>
 )
